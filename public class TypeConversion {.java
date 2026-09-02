0931dee5367e56a872119f63ff1d6f0cf06c096b
preethidevaraja17-crypto <preethidevaraja17@gmail.com>
2026-09-02 10:28:52 +0530

    Add files via upload

---
+++ public class TypeConversion {.java
@@ -0,0 +1,13 @@
+public class TypeConversion {
+    public static void main(String[] args) {
+
+        // Type Conversion (Widening Conversion)
+        int x = 10;
+        float y;
+
+        y = x;
+
+        System.out.println("Value of x = " + x);
+        System.out.println("Value of y = " + y);
+    }
+}
