eded8052f35c0ff24ac123fd47cae637849239af
preethidevaraja17-crypto <preethidevaraja17@gmail.com>
2026-09-08 11:28:28 +0530

    Add files via upload

---
+++ public class digit {.java
@@ -0,0 +1,12 @@
+public class digit {
+    public static void main (String args[]){
+        int num= 2598;
+        int rev=0;
+        while(num!=0){
+            int digit= num%10;
+            rev= rev*10+digit;
+            num= num/10;
+        }
+        System.out.println("Reversed number: " + rev);
+    }
+}
